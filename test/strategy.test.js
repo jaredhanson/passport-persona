0e090bff901350bf84e2578b1dedc95e85c53d4d
Jared Hanson <jaredhanson@gmail.com>
2016-11-19 10:37:35 -0800

    Add test case.

--- test/strategy.test.js
+++ test/strategy.test.js
@@ -527,6 +527,75 @@ describe('Strategy', function() {
     });
   }); // handling a request with an assertion that fails verification due to audience mismatch
   
+  describe('handling a request with an assertion that fails verification due to expired assertion', function() {
+    var mockhttps = {
+      request : function(options, callback) {
+        var req = new MockRequest();
+        var res = new MockResponse();
+        
+        req.on('end', function(data, encoding) {
+          if (options.method !== 'POST') { return res.emit('error', new Error('incorrect options.method argument')); }
+          if (options.headers['Content-Type'] !== 'application/x-www-form-urlencoded') { return res.emit('error', new Error('incorrect options.headers argument')); }
+          if (options.headers['Content-Length'] !== 70) { return res.emit('error', new Error('incorrect options.headers argument')); }
+          if (data !== 'assertion=secret-assertion-data&audience=https%3A%2F%2Fwww.example.com') { return res.emit('error', new Error('incorrect data argument')); }
+          
+          res.emit('data', JSON.stringify({ status: 'failure', reason: 'assertion has expired' })
+          );
+          res.emit('end');
+        })
+        
+        callback(res);
+        return req;
+      }
+    }
+    
+    var strategy = new PersonaStrategy({
+        audience: 'https://www.example.com',
+        transport: mockhttps
+      },
+      function(email, done) {
+        done(null, { email: email });
+      }
+    );
+    
+    
+    var error
+      , user
+      , info;
+    
+    before(function(done) {
+      chai.passport.use(strategy)
+        .success(function(u, i) {
+          user = u;
+          info = i;
+          done();
+        })
+        .req(function(req) {
+          req.body = {};
+          req.body['assertion'] = 'secret-assertion-data';
+        })
+        .error(function(err) {
+          error = err;
+          done();
+        })
+        .authenticate();
+    });
+
+    it('should error', function() {
+      expect(error).to.be.an.instanceof(Error)
+      expect(error).to.be.an.instanceof(VerificationError)
+      expect(error.message).to.equal('assertion has expired')
+    });
+
+    it('should not yield user', function() {
+      expect(user).to.be.undefined;
+    });
+    
+    it('should not yeild info', function() {
+      expect(info).to.be.undefined;
+    });
+  }); // handling a request with an assertion that fails verification due to expired assertion
+  
   describe('encountering an unexpected response from verify endpoint', function() {
     var mockhttps = {
       request : function(options, callback) {
